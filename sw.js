6e28062e1eef42677bc5b5f081a08834ba33a392
Xlentz <tjob@mail.de>
2026-04-23 21:56:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const CORE_ASSETS = [
 
 // Versuche nur Kategorien zu laden, die wahrscheinlich existieren (z.B. 1-10)
 const CATEGORY_ASSETS = [];
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 20; i++) {
   CATEGORY_ASSETS.push(`./category-${i}.json`);
 }
 
